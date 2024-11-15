6dea126dccbc7480380769a427ee0bb07fd0a731
PilotJsauce <Jbsoucy26@gmail.com>
2024-11-14 20:04:16 -0500

    finished database connection, succussful get and post request on postman, routes folder updated, cors installed

---
+++ BackEnd/routes/userRoutes.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import User from '../models/userSchema.js';
+const router = Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ message: 'Error creating user', error: err.message });
+  }
+});
+
+export default router;
