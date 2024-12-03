c1f3a75c17788e8c2c107289041fe25b3921f664
PilotJsauce <Jbsoucy26@gmail.com>
2024-12-02 19:51:32 -0500

    created Import createBrowserRouter

--- ReactClient/src/App.jsx
+++ ReactClient/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -8,29 +8,40 @@ import Search from "./pages/Search";
 import OurMission from "./pages/OurMission";import RouteComponent from './components/RouteComponent';
 
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/browse",
+    element: <Browse />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+  },
+  {
+    path: "/our-mission",
+    element: <OurMission />,
+  },
+]);
 
+function App() {
   return (
-    <Router>
-      <RouteComponent></RouteComponent><div className="min-h-screen flex flex-col">
+    <RouterProvider router={router}>
+      <RouteComponent />
+      <div className="min-h-screen flex flex-col">
         <Header />
         
-        {/* using Router because the previous setup i had only allowed for one page to be rendered ever */}
         <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/browse" element={<Browse />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/our-mission" element={<OurMission />} />
-          </Routes>
         </main>
 
         <Footer />
       </div>
-    </Router>
+    </RouterProvider>
   );
 }
 
 export default App;
-
 
