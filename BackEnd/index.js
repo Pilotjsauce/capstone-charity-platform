d81781d728d62148b1a93b1d3dc7e623baafcf47
Garrett Kennedy <garrett.kennedy2@snhu.edu>
2024-12-27 21:39:55 -0500

    restored files

---
+++ BackEnd/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import charityRoutes from "./routes/charityRoutes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+// Initialize Express
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true, 
+  })
+);
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}))
+
+app.use(userRoutes);
+app.use(authRoutes);
+app.use(charityRoutes);
+
+// Connect to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {});
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit with failure code
+  }
+};
+
+connectDB();
+
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
